f7364bcc2012cfae827627462ccfc6d88fc26836
duhowise <duhwise@gmail.com>
2019-12-06 14:22:30 +0000

    Adding Grpc to asp.net core

---
+++ src/MeterReader/MeterReaderWeb/Services/MeterService.cs
@@ -0,0 +1,56 @@
+﻿using Grpc.Core;
+using MeterReaderWeb.Data;
+using MeterReaderWeb.Data.Entities;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace MeterReaderWeb.Services
+{
+    public class MeterService : MeterReadingService.MeterReadingServiceBase
+    {
+        private readonly ILogger<MeterService> logger;
+        private readonly IReadingRepository repository;
+
+        public MeterService(ILogger<MeterService> logger, IReadingRepository repository)
+        {
+            this.logger = logger;
+            this.repository = repository;
+        }
+
+        public override async Task<StatusMessage> AddReading(ReadingPacket request, ServerCallContext context)
+        {
+            var result = new StatusMessage
+            {
+                Successful = ReadingStatus.Failure
+            };
+
+            if (request.Successful == ReadingStatus.Success)
+            {
+                try
+                {
+                    foreach (var item in request.Readings)
+                    {
+                        //save to the database
+                        var reading = new MeterReading()
+                        {
+                            Value = item.ReadingValue,
+                            ReadingDate = item.ReadingTime.ToDateTime()
+                        };
+                        repository.AddEntity(reading);
+                    }
+                    if (await repository.SaveAllAsync())
+                    {
+                        result.Successful = ReadingStatus.Success;
+                    }
+                }
+                catch (System.Exception exception)
+                {
+                    result.Message = exception.Message;
+                    logger.LogError($"Exception thrown during saving of readings: {exception}");
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- src/MeterReader/MeterReaderWeb/Startup.cs
+++ src/MeterReader/MeterReaderWeb/Startup.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using MeterReaderLib;
 using MeterReaderWeb.Data;
+using MeterReaderWeb.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -15,66 +11,68 @@ using Microsoft.Extensions.Hosting;
 
 namespace MeterReaderWeb
 {
-  public class Startup
-  {
-    private readonly IConfiguration _config;
-
-    public Startup(IConfiguration config)
+    public class Startup
     {
-      _config = config;
-    }
+        private readonly IConfiguration _config;
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddScoped<JwtTokenValidationService>();
-      services.AddAuthentication()
-        .AddJwtBearer(cfg =>
+        public Startup(IConfiguration config)
         {
-          cfg.TokenValidationParameters = new MeterReaderTokenValidationParameters(_config);
-        });
+            _config = config;
+        }
 
-      services.AddDbContext<ReadingContext>(options =>
-          options.UseSqlServer(
-              _config.GetConnectionString("DefaultConnection")));
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<JwtTokenValidationService>();
+            services.AddAuthentication()
+              .AddJwtBearer(cfg =>
+              {
+                  cfg.TokenValidationParameters = new MeterReaderTokenValidationParameters(_config);
+              });
 
-      services.AddDefaultIdentity<IdentityUser>()
-        .AddEntityFrameworkStores<ReadingContext>();
+            services.AddDbContext<ReadingContext>(options =>
+                options.UseSqlServer(
+                    _config.GetConnectionString("DefaultConnection")));
 
-      services.AddControllersWithViews();
-      services.AddRazorPages();
+            services.AddDefaultIdentity<IdentityUser>()
+              .AddEntityFrameworkStores<ReadingContext>();
 
-      services.AddScoped<IReadingRepository, ReadingRepository>();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddScoped<IReadingRepository, ReadingRepository>();
+            services.AddGrpc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<MeterService>();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
-      app.UseHttpsRedirection();
-      app.UseStaticFiles();
-
-      app.UseRouting();
-
-      app.UseAuthentication();
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllerRoute(
-                  name: "default",
-                  pattern: "{controller=Home}/{action=Index}/{id?}");
-        endpoints.MapRazorPages();
-      });
-    }
-  }
 }
